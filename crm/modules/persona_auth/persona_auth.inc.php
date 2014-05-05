9f4f66d44d9fc2bc8fe8a7d19df56f11bfd386b7
wrought <mattsenate@gmail.com>
2014-05-04 20:16:32 -0700

    attempts to add js, getting funny redirect issue and other bugs

--- crm/modules/persona_auth/persona_auth.inc.php
+++ crm/modules/persona_auth/persona_auth.inc.php
@@ -38,6 +38,11 @@ require_once('Auth/BrowserID.php');
 require_once($crm_root . '/modules/user/user.inc.php');
 
 /**
+ * Initialize variables
+ */
+$email = NULL;
+
+/**
  * Handle persona-based login request.
  *
  * @return the url to display when complete.
@@ -66,6 +71,7 @@ function command_persona_auth_login () {
     $user = $users[0];
     $verifier = new Auth_BrowserID();
     $result = $verifier->verifyAssertion($_POST['assertion']);
+    $email = $result->email;
 
     if ($result->status === 'okay') {
         $valid = true;
@@ -91,6 +97,7 @@ function command_persona_auth_login () {
 */
 function persona_auth_login_form () {
     $form = array(
+        'name' => 'persona_auth_login_form',
         'type' => 'form',
         'method' => 'post',
         'command' => 'persona_auth_login',
@@ -100,7 +107,6 @@ function persona_auth_login_form () {
                 'label' => 'Log in with Persona',
                 'fields' => array(
                     array(
-                        'id' => 'assertion-field',
                         'type' => 'hidden',
                         'name' => 'assertion',
                         'value' => ''
@@ -131,6 +137,32 @@ function theme_persona_auth_login_form () {
 }
 
 /**
+ * Javscript for persona
+ */
+
+$persona_js = '<script src="https://login.persona.org/include.js"></script>';
+
+$navigator_js = "<script>
+navigator.id.watch({
+onlogin: function (assertion) {
+var assertion_field = document.getElementsByName(\"assertion\")[0];
+assertion_field.value = assertion;
+var login_form = document.getElementsByName(\"persona_auth_login_form\")[0];
+//login_form.submit();
+},
+onlogout: function () {
+//window.location = '?command=logout';
+}
+});
+</script>";
+
+$signin_js = "<script>
+var signinLink = document.getElementsByName('submitted')[0];
+if (signinLink) {
+  signinLink.onclick = function() { navigator.id.request(); };
+}</script>";
+
+/**
  * Page hook.  Adds ser module content to a page before it is rendered.
  *
  * @param &$page_data Reference to data about the page being rendered.
@@ -138,10 +170,16 @@ function theme_persona_auth_login_form () {
  * @param $options The array of options passed to theme('page').
 */
 function persona_auth_page (&$page_data, $page_name, $options) {
+    global $persona_js;
+    global $navigator_js;
+    global $signin_js;
 
     switch ($page_name) {
 
-        case 'login':
+        case 'persona_auth_login':
+            page_add_content_top($page_data, $signin_js);
+            page_add_content_top($page_data, $navigator_js);
+            page_add_content_top($page_data, $persona_js);
             page_add_content_top($page_data, theme('form', crm_get_form('persona_auth_login')));
             break;
     }
